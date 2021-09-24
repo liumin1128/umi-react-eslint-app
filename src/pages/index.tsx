d9f85801585f3a9628a4d9e221e3ba1d18e484cc
liumin <970568830@qq.com>
2021-09-25 06:55:00 +0800

    feat: font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import styles from './index.less';
+import Button from '@mui/material/Button';
 
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <Button>test</Button>
     </div>
   );
 }
