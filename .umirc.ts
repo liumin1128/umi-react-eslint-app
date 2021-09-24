d7850d0b21cacc8587877a615793c4125a6ca07f
liumin <970568830@qq.com>
2021-09-24 08:25:07 +0800

    first commit

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'umi';
+// import routes from './src/configs/routes';
+// import theme from './src/configs/theme';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  hash: true,
+  history: { type: 'hash' },
+  // routes: routes,
+  fastRefresh: {},
+  // theme: theme,
+  publicPath: './',
+});
